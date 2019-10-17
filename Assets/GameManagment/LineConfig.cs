5722dea3dde84e0e788d6d7587d2d013b6663ffe
gantar22 <nathancarterwilliams@gmail.com>
2019-10-17 19:05:16 -0400

    adding config

---
+++ Assets/GameManagment/LineConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName="Boogieman/Config")]
+public class LineConfig : ScriptableObject
+{
+    public float speed = 1;
+    public float stepSize = 1;
+}
