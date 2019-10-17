928f4f1344db3fecdab54a928a19745ac675b402
gantar22 <nathancarterwilliams@gmail.com>
2019-10-17 19:01:39 -0400

    literally gitignore

---
+++ Assets/BoilerPlate/Reference.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Reference<T> : ScriptableObject
+{
+    [SerializeField] private T value;
+    public T val
+    {
+        get { return value;}
+        set { this.value = value; }
+    }
+    
+}
