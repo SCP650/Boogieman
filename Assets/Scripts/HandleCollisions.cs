b424470a976bca6447e38e3fbd856b72447e41af
gantar22 <nathancarterwilliams@gmail.com>
2019-10-17 20:09:20 -0400

    Boogieman Bugs

--- Assets/Scripts/HandleCollisions.cs
+++ Assets/Scripts/HandleCollisions.cs
@@ -21,16 +21,22 @@ public class HandleCollisions : MonoBehaviour
     void Start()
     {
         lr = GetComponent<LineRenderer>();
+        
+    }
+
+
+    public void Setup(Vector3[] new_points,ControllerObject new_controller)
+    {
+        this.points = new_points;
+        
+        controller = new_controller;
         if (controller == controllers.leftHand)
             otherController = controllers.rightHand;
         if (controller == controllers.rightHand)
             otherController = controllers.rightHand;
-    }
-
-
-    public void Setup(Vector3[] new_points)
-    {
-        this.points = new_points;
+        
+        
+        StartCoroutine(CheckCollisions());
     }
 
 
