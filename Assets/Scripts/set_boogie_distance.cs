26682bd7dfbcdff01fa86838333bbefbe79aacff
Laptop <>
2019-10-22 22:24:21 -0400

    Finalized proottype

--- Assets/Scripts/set_boogie_distance.cs
+++ Assets/Scripts/set_boogie_distance.cs
@@ -6,12 +6,13 @@ public class set_boogie_distance : MonoBehaviour
 {
     [SerializeField]
     music_config m_config;
+    float measure_delay = 2;
 
     [SerializeField] private LineConfig l_config;
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = Vector3.forward * l_config.speed * 60 * (1 / m_config.bpm) * m_config.beats_per_measure;
+        transform.position = Vector3.forward * l_config.speed * 60 * (1 / m_config.bpm) * m_config.beats_per_measure * measure_delay;
     }
 
     // Update is called once per frame
