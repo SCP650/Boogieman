f8ee2f2c6982c19fd999628698398690cf30d750
Pipdoodle <llo1@andrew.cmu.edu>
2019-11-07 18:49:01 -0500

    discoFloor Fix

--- Assets/RoomPackage/Effects/discoTile.cs
+++ Assets/RoomPackage/Effects/discoTile.cs
@@ -5,22 +5,22 @@ using UnityEngine;
 public class discoTile : MonoBehaviour
 {
     private Color[] colors;
-    private Color[] emitColor;
+    private Color[] emitColors;
     [SerializeField] float bpm;
 
     // Start is called before the first frame update
     void Start()
     {
         colors = new Color[7];
-        colors[0] = new Color(0f, 85f, 120f);
+        colors[0] = new Color(209f, 0f, 0f);
         colors[1] = new Color(0f, 55f, 0f);
         colors[2] = new Color(85f, 0f, 30f);
         colors[3] = new Color(20f, 0f, 80f);
         colors[4] = new Color(50f, 20f, 0f);
         colors[5] = new Color(7, 55f, 0f);
         colors[6] = new Color(50f, 0f, 0f);
-        emitColor = new Color[7];
-        colors[0] = new Color(0f, 125f, 150f);
+        emitColors = new Color[7];
+        colors[0] = Color.HSVToRGB(0f, 21, 75f);
         colors[1] = new Color(0f, 85f, 0f);
         colors[2] = new Color(125f, 0f, 60f);
         colors[3] = new Color(0f, 0f, 120f);
@@ -36,9 +36,10 @@ public class discoTile : MonoBehaviour
         {
             int val = Random.Range(0, 7);
             Color color = colors[val];
+            Color emitColor = emitColors[0];
             this.GetComponent<Renderer>().material.SetColor("_Color", color);
-            //this.GetComponent<Renderer>().material.SetColor("_EmissionColor", color);
-            yield return new WaitForSeconds(1f/(110f/60f));
+            //this.GetComponent<Renderer>().material.SetColor("_EmissionColor", new Color(191f, 1501, 150f));
+            yield return new WaitForSeconds(1f/(bpm/60f));
         }
         yield return null;
 
--- Assets/RoomPackage/Effects/spin.cs
+++ Assets/RoomPackage/Effects/spin.cs
@@ -6,9 +6,14 @@ public class spin : MonoBehaviour
 {
     [SerializeField] float speed=100f;
     [SerializeField] float direction = 1f;
+    [SerializeField] float time = 0f;
     // Start is called before the first frame update
     void Start()
     {
+        if (time > 0)
+        {
+            StartCoroutine(switchDirection());
+        }
         
     }
 
@@ -16,6 +21,17 @@ public class spin : MonoBehaviour
     void Update()
     {
         this.transform.Rotate(new Vector3(0, direction * speed * Time.deltaTime, 0), Space.World);
+
+    }
+
+    IEnumerator switchDirection()
+    {
+        while (true)
+        {
+            direction = -direction;
+            yield return new WaitForSeconds(time);
+        }
+        yield return null;
 
     }
 }
