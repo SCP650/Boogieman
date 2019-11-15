4aa4dfb60ab9215c74117898624442979a7ab115
Pipdoodle <llo1@andrew.cmu.edu>
2019-11-14 19:13:08 -0500

    changes

--- Assets/RoomPackage/Effects/TileMap.cs
+++ Assets/RoomPackage/Effects/TileMap.cs
@@ -2,20 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class TileMap : MonoBehaviour
 {
     [SerializeField] GameObject [] tiles;
+    Material[] materials;
     private string pattern;
     private Color[] colors;
     private Color[] options;
     private int startVal = 0;
     [SerializeField] Light[] lights;
+    [SerializeField] UnitEvent beat;
 
+  
 
     // Start is called before the first frame update
     void Start()
     {
-        pattern = "center";
+        beat.AddListener(unit => change());
+        pattern = "checkered";
         colors = new Color[7];
         options = new Color[7];
         options[0] = new Color(255f, 0f, 0f);
@@ -25,16 +30,20 @@ public class TileMap : MonoBehaviour
         options[4] = new Color(0, 251f, 255f);
         options[5] = new Color(214, 0f, 255f);
         options[6] = new Color(255f, 0f, 188f);
-        centerPattern();
-        //tiles = new GameObject[75];
+        changeCheckered();
+        materials = new Material[75];
+        int count = 0;
+        foreach (GameObject tile in tiles)
+        {
+            materials[count] = new Material(tile.GetComponent<Renderer>().sharedMaterial);
+            tile.GetComponent<Renderer>().material = materials[count];
+            count += 1;
+        }
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 
     public void change()
     {
@@ -43,15 +52,18 @@ public class TileMap : MonoBehaviour
         if (pattern == "checkered")
         {
             int currCol = 0;
+            int count = 0;
             foreach (GameObject tile in tiles)
             {
                 lights = tile.GetComponentsInChildren<Light>();
-                tile.GetComponent<Renderer>().material.SetColor("_Color", colors[currCol %2]);
+                //tile.GetComponent<Renderer>().material.SetColor("_Color", colors[currCol %2]);
+                materials[count].SetColor("_Color", colors[currCol % 2]);
                 foreach (Light l in lights)
                 {
                     l.color = colors[currCol % 2];
                 }
                 currCol += 1;
+                count += 1;
             }
             Color temp = colors[0];
             colors[0] = colors[1];
@@ -60,15 +72,18 @@ public class TileMap : MonoBehaviour
         else if (pattern == "spin")
         {
             int currCol = 0;
+            int count = 0;
             foreach (GameObject tile in tiles)
             {
                lights = tile.GetComponentsInChildren<Light>();
-               tile.GetComponent<Renderer>().material.SetColor("_Color", colors[(currCol + startVal) % 7]);
-               foreach (Light l in lights)
+                //tile.GetComponent<Renderer>().material.SetColor("_Color", colors[(currCol + startVal) % 7]);
+                materials[count].SetColor("_Color", colors[(currCol + startVal) % 7]);
+                foreach (Light l in lights)
                 {
                     l.color = colors[(currCol + startVal) % 7];
                 }
                 currCol += 1;
+                count += 1;
             }
             //startVal += 1;
 
@@ -82,7 +97,8 @@ public class TileMap : MonoBehaviour
                 lights = tile.GetComponentsInChildren<Light>();
                 if (tileNum < 36)
                 {
-                    tile.GetComponent<Renderer>().material.SetColor("_Color", colors[0]);
+                    //tile.GetComponent<Renderer>().material.SetColor("_Color", colors[0]);
+                    materials[tileNum].SetColor("_Color", colors[0]);
                     foreach (Light l in lights)
                     {
                         l.color = colors[0];
@@ -91,21 +107,24 @@ public class TileMap : MonoBehaviour
                 }
                 else if (tileNum < (28 + 36))
                 {
-                    tile.GetComponent<Renderer>().material.SetColor("_Color", colors[1]);
+                    materials[tileNum].SetColor("_Color", colors[1]);
+                   // tile.GetComponent<Renderer>().material.SetColor("_Color", colors[1]);
                     foreach (Light l in lights)
                     {
                         l.color = colors[1];
                     }
                 }
                 else if (tileNum < (11 + 28 + 36))
-                { 
-                    tile.GetComponent<Renderer>().material.SetColor("_Color", colors[2]);
+                {
+                    materials[tileNum].SetColor("_Color", colors[2]);
+                    //tile.GetComponent<Renderer>().material.SetColor("_Color", colors[2]);
                     foreach (Light l in lights)
                     {
                         l.color = colors[2];
                     }
                 }
                 tileNum += 1;
+             
             }
             Color temp = colors[0];
             colors[0] = colors[2];
@@ -115,7 +134,7 @@ public class TileMap : MonoBehaviour
     }
 
 
-    public void chngeCheckered()
+    public void changeCheckered()
     {
         int val = Random.Range(0, 7);
         colors[0] = options[val];
