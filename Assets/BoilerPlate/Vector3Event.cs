16866e34dc12f202d5e8efe99cf398074f791593
Laptop <>
2019-11-18 11:46:49 -0500

    Vec3 Event

--- Assets/BoilerPlate/Vector3Event.cs
+++ Assets/BoilerPlate/Vector3Event.cs
@@ -3,4 +3,5 @@ using UnityEngine.Events;
 
 
 public class Vec3UnityEvent : UnityEvent<Vector3> {}
+[CreateAssetMenu(menuName = "Framework/Events/Vector3")]
 public class Vector3Event : EventObject<Vector3,Vec3UnityEvent> {}
---
+++ Assets/Scripts/NewGenerateSphere.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewGenerateSphere : MonoBehaviour
+{
+    //this script should be attached to the center of the game object you want spheres to generate from -- Sebastian Yang, or is it?
+    [SerializeField] Vec3UnityEvent spherelocation;
+    [SerializeField] GameObject SpherePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spherelocation.AddListener(GiveMeSphere);
+    }
+    
+    void GiveMeSphere(Vector3 location)
+    {
+        Instantiate(SpherePrefab, location, Quaternion.identity);
+    }
+}
