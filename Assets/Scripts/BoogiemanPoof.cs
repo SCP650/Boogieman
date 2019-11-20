b234c7729793bc44af7b7deca412abb15c0f3356
Laptop <>
2019-11-19 20:49:48 -0500

    poof added
    for the duration of poof session, boogieman is at given location doing things

---
+++ Assets/Scripts/BoogiemanPoof.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoogiemanPoof : MonoBehaviour
+{
+    [SerializeField]
+    private Session poof;
+
+    [SerializeField]
+    private GameObject boogieMan;
+
+    void Start()
+    {
+        //StartCoroutine(InvokePoofEvent());
+        poof.AddStartListener(() => StartCoroutine(PoofInBoogieman()));
+        poof.AddStopListener(() => StartCoroutine(PoofOutBoogieman()));
+    }
+
+    IEnumerator PoofInBoogieman()
+    {
+        boogieMan.SetActive(true);
+        yield return null;
+    }
+
+    IEnumerator PoofOutBoogieman()
+    {
+        boogieMan.SetActive(false);
+        yield return null;
+    }
+
+    // IEnumerator InvokePoofEvent()
+    // {
+    //     while(true)
+    //     {
+    //         if(Input.GetKeyDown("p"))
+    //         {
+    //             poof.Invoke();
+    //         }
+
+    //         yield return null;
+    //     }
+    // }
+}
