8ab347fed49c51379654b1eed18a7aa04302911b
gantar22 <nathancarterwilliams@gmail.com>
2019-12-01 19:23:06 -0500

    new attack

---
+++ Assets/point_testser.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class point_testser : MonoBehaviour
+{
+[SerializeField] 
+AnimationCurve c;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
