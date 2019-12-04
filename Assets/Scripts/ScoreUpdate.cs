8d7732cff52120fe99dddfbf7c25b60abb1d2803
hitechlife <kanosmask@gmail.com>
2019-12-04 11:52:13 -0500

    Added simple scoreupdate script skeleton

---
+++ Assets/Scripts/ScoreUpdate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreUpdate : MonoBehaviour
+{
+    [SerializeField] IntEvent ScorePoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScorePoint.AddListener(UpdateScore);
+    }
+
+    // Update is called once per frame
+    void UpdateScore()
+    {
+        
+    }
+}
