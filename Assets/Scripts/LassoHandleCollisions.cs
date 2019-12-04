c4ef6fd5eff6c8b15db580c7bc38b05f3d8f3202
Sebastian Yang <yukaiy@andrew.cmu.edu>
2019-12-04 11:49:32 -0500

    Lasso Generation & Collision Check

---
+++ Assets/Scripts/LassoHandleCollisions.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using System.Linq;
+using TMPro;
+using Valve.VR;
+
+[RequireComponent(typeof(LineRenderer))]
+public class LassoHandleCollisions : MonoBehaviour
+{
+    private LineRenderer lr;
+    [SerializeField] private ParticleSystem goodParticlePrefab;
+    [SerializeField] private ParticleSystem badParticlePrefab;
+    [SerializeField] private LineConfig config;
+    private ControllerObject RightController;
+    private ControllerObject LeftController;
+    [SerializeField] private controllerSet controllers;
+    [SerializeField] private IntEvent ScorePoint;
+    [SerializeField] private LineRenderer lr_prefab;
+    [SerializeField] private GameObject endIndicator;
+    [SerializeField] private SteamVR_Action_Vibration HapticAction;
+
+    private SteamVR_Input_Sources leftControllerHand, rightControllerHand, hapticController;
+    private Vector3[] points;
+    
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        points = new Vector3[0];
+        lr = GetComponent<LineRenderer>();
+        if (lr == null) Debug.LogError("Why isn't there a line renderer");
+        leftControllerHand = SteamVR_Input_Sources.LeftHand;
+        rightControllerHand = SteamVR_Input_Sources.RightHand;
+      
+    }
+
+
+    public void Setup(Vector3[] new_points)
+    {
+        this.points = new_points;
+        lr.positionCount = points.Length;
+        lr.SetPositions(points);
+
+        endIndicator = Instantiate(endIndicator,transform);
+        var _MeshRenderer = endIndicator.GetComponent<MeshRenderer>();
+     
+            lr_prefab.startColor = (Color.blue);
+            lr_prefab.endColor = Color.blue;
+  
+        StartCoroutine(CheckCollisions());
+        StartCoroutine(set_hitable());
+    }
+
+
+    IEnumerator set_hitable()
+    {
+        while (true)
+        {
+            yield return null;
+            int i = 0;
+            for(; i < points.Length;i++)
+            {
+                if (points[i].z + transform.position.z < config.hit_threshold)
+                {
+                    break;
+                }
+
+            }
+            
+            var grad = new Gradient();
+            GradientAlphaKey[] ks = new[]
+            {
+                new GradientAlphaKey(.2f,0), 
+                new GradientAlphaKey(.2f, i / (float) points.Length - .05f),
+                new GradientAlphaKey(1, i / (float) points.Length +.05f),
+                new GradientAlphaKey(1,1) 
+            };
+            grad.SetKeys(lr.colorGradient.colorKeys,ks);
+            lr.colorGradient = grad;
+        }
+    }
+
+    //TODO: delete and reset points array when removing points
+    IEnumerator CheckCollisions()
+    {
+        while (true)
+        {
+            for (int i = 0; i < points.Length; ++i)
+            {
+                var point = points[i] + transform.position;
+                if (CheckController(point, config.correctHandGrace))
+                {
+                    if(config.hit_from_the_end_only && i != points.Length - 1)
+                        continue;
+
+                  
+                    Instantiate(goodParticlePrefab, point,Quaternion.identity);
+                    HapticAction.Execute(0, 0.2f, 30, 0.5f, leftControllerHand);
+                    HapticAction.Execute(0, 0.2f, 30, 0.5f, rightControllerHand);
+                    StartCoroutine(DestroyRest(i + 1));
+                    ScorePoint.Invoke(1);
+                    break;
+                }
+                if ( LeftController != null && RightController != null) //our controller is not the head
+                {
+                    if(config.hit_from_the_end_only)
+                        continue;
+                    
+                    if (CheckController(point, config.incorrectHandGrace))
+                    {
+                        StartCoroutine(DestroyRest(i));
+                        break;
+                    }
+                }
+            }
+
+            if (points.Length == 0)
+            {
+                Destroy(endIndicator);
+                Destroy(gameObject);
+                yield break;
+            }
+            endIndicator.transform.localPosition = points.Last();
+            yield return null;
+        }
+
+    }
+
+    IEnumerator DestroyRest(int j)
+    {
+        float speed = 5;
+
+
+        var new_points = points.Skip(j).ToArray();
+
+        
+        points = points.Take(j - 1).ToArray();
+        
+        lr.positionCount = points.Length;
+        lr.SetPositions(points);
+        if(new_points.Length == 0)
+            yield break;
+        
+        var new_line = Instantiate(lr_prefab,transform,false);
+        new_line.positionCount = new_points.Count();
+        new_line.SetPositions(new_points);
+        var magic_wand = Instantiate(badParticlePrefab, new_points[0] + transform.position, Quaternion.identity);
+        
+        for (; new_points.Length > 0;new_points = new_points.Skip(1).ToArray())
+        {
+            var dest = new_points[0] + transform.position;
+            while (Vector3.Distance(magic_wand.transform.position, dest) > .1f)
+            {
+                magic_wand.transform.position += (dest - magic_wand.transform.position).normalized * speed * Time.deltaTime;
+                yield return null;
+            }
+            new_line.positionCount = new_points.Count();
+            new_line.SetPositions(new_points);
+        }
+        Destroy(new_line);
+    }
+
+    bool CheckController( Vector3 position,float dist)
+    {
+        return Vector3.Distance(LeftController.pos, position) < dist && Vector3.Distance(rightControllerHand.pos, position) < dist; 
+    }
+    
+ 
+}
--- Assets/Scripts/NewGenerateSphere.cs
+++ Assets/Scripts/NewGenerateSphere.cs
@@ -8,7 +8,6 @@ public class NewGenerateSphere : MonoBehaviour
     [SerializeField] Vec3UnityEvent spherelocation;
     [SerializeField] GameObject SpherePrefab;
 
-
     // Testing
     //TODO: use existing Calibration script
     public float width; // the width of player's arm streach out
@@ -19,26 +18,39 @@ public class NewGenerateSphere : MonoBehaviour
     [SerializeField] private ControllerObject head;
     [SerializeField] Material left;
     [SerializeField] Material right;
+    [SerializeField] GameObject lineObject;
     [SerializeField] AttackController leftAttackController;
     [SerializeField] AttackController rightAttackController;
+    [SerializeField] LineConfig config;
+
     private float scaler = 0.8f;
     // [SerializeField] Vector3Ref rightAttackPos; // this is confusing, this should be both left and right attack pos
-    
+    LineRenderer lr;
     private int counter = 0;
     public int numBeatWait = 4;
     private ControllerObject currController;
+    private LassoGenerateLine generateLine;
 
+    float runtime = .55f;
 
+    bool recording;
+    Vector3[] positions;
+    public Vector3 initPos;
+    
     private void Start()
     {
         heights = new List<float>();
         currController = righthand;
-
+        generateLine = GetComponent<LassoGenerateLine>();
+        positions = new Vector3[0];
         //TODO: add in lasso and line generation here
         leftAttackController.BallSession.AddStartListener(() => GiveMeSphere(leftAttackController.Place.val, lefthand));
         rightAttackController.BallSession.AddStartListener(() => GiveMeSphere(rightAttackController.Place.val, righthand));
-        // ballSession.AddStartListener(() => GiveMeSphere(rightAttackPos.val));
-        // ballSession.AddStopListener(() => GiveMeSphere(rightAttackPos.val));
+        leftAttackController.LassoSession.AddStartListener(() => {initPos = AdjustInitPosition(leftAttackController.Place.val) ; recording = true; StopAllCoroutines(); StartCoroutine(Record(initPos)); });
+        leftAttackController.LassoSession.AddStopListener(() => recording = false);
+    
+    // ballSession.AddStartListener(() => GiveMeSphere(rightAttackPos.val));
+    // ballSession.AddStopListener(() => GiveMeSphere(rightAttackPos.val));
         StartCoroutine(startCalibration());
     }
 
@@ -50,6 +62,11 @@ public class NewGenerateSphere : MonoBehaviour
         Debug.Log(height);
     }
 
+    private Vector3 AdjustInitPosition(Vector3 location)
+    {
+        return new Vector3(location.x * width / 2.0f + transform.position.x, location.y * height, transform.position.z);
+
+    }
 
     private float ListAverage(List<float> temp)
     {
@@ -76,11 +93,11 @@ public class NewGenerateSphere : MonoBehaviour
     
     void GiveMeSphere(Vector3 location, ControllerObject cObject)
     {
-        Debug.Log("giving me sphere");
-        Vector3 v = new Vector3(location.x * width/2.0f + transform.position.x, location.y * height/2.0f + height/2.0f*(5/4f) + transform.parent.position.y, transform.position.z);
+
+        initPos = AdjustInitPosition(location);
         currController = cObject;
 
-        var block = Instantiate(SpherePrefab, v, Quaternion.identity, null);
+        var block = Instantiate(SpherePrefab, initPos, Quaternion.identity, null);
         block.GetComponent<HandleBlockCollisions>().Setup(currController);
         Light bL = block.GetComponent<Light>();
         var _MeshRenderer = block.GetComponent<MeshRenderer>();
@@ -95,5 +112,60 @@ public class NewGenerateSphere : MonoBehaviour
             _MeshRenderer.material = right;
         }
     }
+
+
+
+    void stopListener()
+    {
+        Debug.Log("stop invoked");
+        StopAllCoroutines();
+
+        if (positions.Length > 3)
+        {
+            GameObject line = Instantiate(lineObject, Vector3.zero, Quaternion.identity);
+            //TODO: Handle conlission in line 
+            line.GetComponent<LassoHandleCollisions>().Setup(positions);
+        }
+     
+
+        positions = new Vector3[0];
+        lr.positionCount = 0;
+        lr.SetPositions(positions);
+    }
+
+    IEnumerator Record(Vector3 iniPos)
+    {
+        Debug.Log("starting record");
+
+        for (float dur = 0; dur < runtime; dur += Time.deltaTime)
+        {
+            Vector3[] newPositions = new Vector3[positions.Length + 1];
+            newPositions[0] = iniPos;
+
+            // Update old positions by moving them forward based on how much time has passed
+            for (int i = 1; i < positions.Length + 1; i++)
+            {
+                newPositions[i] = positions[i - 1] + Vector3.back * config.stepSize * config.speed;
+                // yield return null;
+            }
+         
+            positions = newPositions;
+            lr.positionCount = positions.Length;
+            lr.SetPositions(positions);
+
+            yield return new WaitForSeconds(config.stepSize);
+        }
+        stopListener();
+        yield return new WaitForSeconds(runtime);
+        if (recording)
+        {
+            yield return null;
+
+            StartCoroutine(Record(iniPos));
+            yield break;
+        }
+    
+    }
+
 
 }
