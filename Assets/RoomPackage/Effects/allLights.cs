f225038a99b52ec823a64c108b28533d35ce4c19
Pipdoodle <llo1@andrew.cmu.edu>
2019-12-04 23:09:48 -0500

    Added all off functionality

---
+++ Assets/RoomPackage/Effects/allLights.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class allLights : MonoBehaviour
+{
+    [SerializeField] GameObject[] all;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void allOff()
+    {
+        foreach (GameObject gameobject in all)
+        {
+            gameobject.SetActive(false);
+        }
+    }
+    public void allOn ()
+    {
+        foreach (GameObject gameobject in all)
+         {
+            gameobject.SetActive(true);
+        }
+    }
+}
--- Assets/RoomPackage/Effects/discoTile.cs
+++ Assets/RoomPackage/Effects/discoTile.cs
@@ -5,59 +5,71 @@ using UnityEngine;
 public class discoTile : MonoBehaviour
 {
     private Color[] colors;
-    private Color[] emitColors;
-    [SerializeField] float bpm;
+    [SerializeField] bool swap = false;
+    [SerializeField] float bpm = 104f;
     [SerializeField] Light [] lights;
+    private Material materials;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        materials = this.GetComponent<Renderer>().material;
         lights = this.GetComponentsInChildren<Light>();
         colors = new Color[7];
-        colors[0] = new Color(209f, 0f, 0f);
-        colors[1] = new Color(0f, 55f, 0f);
-        colors[2] = new Color(85f, 0f, 30f);
-        colors[3] = new Color(20f, 0f, 80f);
-        colors[4] = new Color(50f, 20f, 0f);
-        colors[5] = new Color(7, 55f, 0f);
-        colors[6] = new Color(50f, 0f, 0f);
-        emitColors = new Color[7];
-        colors[0] = Color.HSVToRGB(0f, 21, 75f);
-        colors[1] = new Color(0f, 85f, 0f);
-        colors[2] = new Color(125f, 0f, 60f);
-        colors[3] = new Color(0f, 0f, 120f);
-        colors[4] = new Color(100f, 70f, 0f);
-        colors[5] = new Color(25f, 85f, 0f);
-        colors[6] = new Color(80f, 0f, 0f);
-        //StartCoroutine(materialChange());
+        colors[0] = new Color(1f, 0f, 0f);
+        colors[1] = new Color(1f, 157 / 255f, 0f);
+        colors[2] = new Color(1f, 1f, 0f);
+        colors[3] = new Color(0f, 1f, 25f / 255f);
+        colors[4] = new Color(0, 251f / 255f, 1f);
+        colors[5] = new Color(214f / 255f, 0f, 1f);
+        colors[6] = new Color(1f, 0f, 188f / 255f);
+
+
+        if (swap)
+        {
+            StartCoroutine(materialChange());
+        }
     }
-    /*public void changeColor()
+
+    private void setTileColor( Color c)
+    {
+        Debug.Log("Change color");
+
+        float h, s, v;
+        Color.RGBToHSV(c, out h, out s, out v);
+        Color desat = Color.HSVToRGB(h, s * 0.3f, v);
+        materials.SetColor("_BaseColor", c);
+        materials.SetVector("_EmissionColor", desat);
+        lights[0].color = c;
+    }
+
+
+    public void changeColor()
     {
         int val = Random.Range(0, 7);
-        Color color = colors[val];
-        Color emitColor = emitColors[0];
-        this.GetComponent<Renderer>().material.SetColor("_Color", color);
-        foreach (Light l in lights)
-        {
-            l.color = colors[val];
-        }
-    }*/
-    /*IEnumerator materialChange()
+        //Color color = colors[val];
+        setTileColor(colors[val]);
+    }
+   IEnumerator materialChange()
     {
         while (true)
         {
-            int val = Random.Range(0, 7);
+            Debug.Log("yes");
+            changeColor();
+             
+            /*int val = Random.Range(0, 7);
             Color color = colors[val];
             Color emitColor = emitColors[0];
             this.GetComponent<Renderer>().material.SetColor("_Color", color);
             foreach (Light l in lights)
             { 
                 l.color = colors[val];
-            }
+            }*/
             //this.GetComponent<Renderer>().material.SetColor("_EmissionColor", new Color(191f, 1501, 150f));
             yield return new WaitForSeconds(1f/(bpm/60f));
         }
-        yield return null;
+        //yield return null;
 
-    }*/
+    }
 }
