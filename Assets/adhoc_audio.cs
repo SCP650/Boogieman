46948a15b77ddfd43637b3480836a6515498316a
gantar22 <nathancarterwilliams@gmail.com>
2019-12-05 20:04:46 -0500

    important things

---
+++ Assets/adhoc_audio.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class adhoc_audio : MonoBehaviour
+{
+
+
+    [SerializeField] public AudioSource ass;
+    
+    [SerializeField]
+    int threshold = 500;
+        
+
+    [SerializeField]        
+    IntRef score;
+        // Start is called before the first frame update
+    void Start()
+    {
+        if(score.val > threshold)
+            ass.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/sum_event.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sum_event : MonoBehaviour
+{
+    [SerializeField] private IntRef result;
+
+    [SerializeField] private IntEvent input;
+    // Start is called before the first frame update
+    void Start()
+    {
+        result.val = 0;
+        input.AddListener(i => result.val += i);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
