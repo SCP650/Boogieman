303846a2b3352012e6150bb826b76c39ff443ddc
gantar22 <nathancarterwilliams@gmail.com>
2019-12-05 22:06:44 -0500

    conflicts

--- Assets/adhoc_audio.cs
+++ Assets/adhoc_audio.cs
@@ -18,14 +18,36 @@ public class adhoc_audio : MonoBehaviour
 
     [SerializeField] private audioBundle[] b;
 
+    [SerializeField] private AudioClip truce;
+    [SerializeField] private AudioClip smellyoulater;
+    
     private int i = 0;
 
     [SerializeField]        
     IntRef score;
         // Start is called before the first frame update
-    void onEnable()
+
+
+
+        void onEnable()
+        {
+            StartCoroutine(p());
+        }
+    public IEnumerator p()    
     {
+        if (i == b.Length - 1)
+        {
+            ass.PlayOneShot(truce);
+            yield return new WaitUntil(() => !ass.isPlaying);
+            yield return new WaitForSeconds(.5f);
+        }
         ass.PlayOneShot(score.val > b[i].threshold ? b[i].good : b[i].bad);
+        if (i == b.Length - 1)
+        {
+            yield return new WaitUntil(() => !ass.isPlaying);
+            yield return new WaitForSeconds(.5f);
+            ass.PlayOneShot(smellyoulater);
+        }
         i++;
         
     }
