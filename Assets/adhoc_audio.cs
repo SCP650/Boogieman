3846979217943a230153b5189684bfc1f8be9689
gantar22 <nathancarterwilliams@gmail.com>
2019-12-05 21:59:43 -0500

    some stuff

--- Assets/adhoc_audio.cs
+++ Assets/adhoc_audio.cs
@@ -1,25 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[System.Serializable]
+public struct audioBundle
+
+{
+[SerializeField] public AudioClip good;
+[SerializeField] public AudioClip bad;
+[SerializeField] public int threshold;
+}
 
 public class adhoc_audio : MonoBehaviour
 {
 
 
     [SerializeField] public AudioSource ass;
-    [SerializeField] public AudioClip good;
-    [SerializeField] public AudioClip bad;
-    
-    [SerializeField]
-    int threshold = 500;
-        
+
+    [SerializeField] private audioBundle[] b;
+
+    private int i = 0;
 
     [SerializeField]        
     IntRef score;
         // Start is called before the first frame update
-    void Start()
+    void onEnable()
     {
-        ass.PlayOneShot(score.val > threshold ? good : bad);
+        ass.PlayOneShot(score.val > b[i].threshold ? b[i].good : b[i].bad);
+        i++;
+        
     }
 
     // Update is called once per frame
