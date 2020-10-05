da39a9ce67c3e814575885cb1ca65517ab943416
Sebastian Yang <yukaiy@andrew.cmu.edu>
2020-10-05 14:55:37 -0400

    new scene

---
+++ Assets/Scripts/VRResearch/generateBlocks.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generateBlocks : MonoBehaviour
+{
+
+    private List<Block> blocks;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //TODO: Adrian: return a list of block that contains block informaion
+        blocks = getblock(songname);
+        StartCoroutine(Generate(blocks));
+    }
+    private IEnumerator Generate(blocks)
+    {
+        foreach(Block B in blocks)
+        {
+            //TODO: intantiate the blocks 
+            yield return new WaitForSeconds(time);
+        }
+    }
+}
