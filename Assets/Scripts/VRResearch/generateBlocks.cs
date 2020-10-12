93ce97b4c7b0e56a565bf89d28c549907cb30d41
Sebastian Yang <yukaiy@andrew.cmu.edu>
2020-10-12 14:46:42 -0400

    generae block

--- Assets/Scripts/VRResearch/generateBlocks.cs
+++ Assets/Scripts/VRResearch/generateBlocks.cs
@@ -7,7 +7,19 @@ using System.IO;
 public class generateBlocks : MonoBehaviour
 {
 
+    [SerializeField] GameObject RedBlock;
+    [SerializeField] GameObject BlueBlock;
     private List<Block> blocks;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //TODO: Adrian: return a list of block that contains block informaion
+       GetBlock("Assets/ExpertPlus.json");
+        StartCoroutine(Generate(blocks));
+    }
+
 
     public void GetBlock(string songname)
     {
@@ -16,22 +28,33 @@ public class generateBlocks : MonoBehaviour
             string song = r.ReadToEnd();
             blocks = JsonConvert.DeserializeObject<List<Block>>(song);
         }
-    }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        //TODO: Adrian: return a list of block that contains block informaion
-        GetBlock("ExpertPlus.json");
-        StartCoroutine(Generate(blocks));
     }
 
     private IEnumerator Generate(List<Block> blocks)
     {
         foreach (Block B in blocks)
         {
+            Debug.Log(B._time);
             //TODO: intantiate the blocks
-            yield return new WaitForSeconds(2);
+            GameObject gb;
+            if (B._type == 0)
+            {
+               gb =  Instantiate(RedBlock);
+            }
+            else
+            {
+                gb = Instantiate(BlueBlock);
+            }
+            gb.transform.position = getPosition(B._lineIndex, B._lineLayer);
+            yield return new WaitForSeconds(B._time);
         }
+    }
+
+    private Vector3 getPosition(int col, int row)
+    {
+        float y = 1.5f + row * 0.5f;
+        float x = -1 + col *0.5f;
+        return new Vector3(x, y, transform.position.z);
     }
 }
