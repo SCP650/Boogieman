7d188eb5a37245b845d5f53ceafe129eb789f6b1
ama28 <adrianm@andrew.cmu.edu>
2020-10-12 13:46:42 -0400

    added Json.NET and block.cs & generateblock.cs

--- Assets/Scripts/VRResearch/generateBlocks.cs
+++ Assets/Scripts/VRResearch/generateBlocks.cs
@@ -1,24 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Newtonsoft.Json;
+using System.IO;
 
 public class generateBlocks : MonoBehaviour
 {
 
     private List<Block> blocks;
+    []
+
+    public void GetBlock(string songname)
+    {
+        using (StreamReader r = File.OpenText(songname))
+        {
+            string song = r.ReadToEnd();
+            blocks = JsonConvert.DeserializeObject<List<Block>>(song);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         //TODO: Adrian: return a list of block that contains block informaion
-        blocks = getblock(songname);
+        GetBlock("ExpertPlus.json");
         StartCoroutine(Generate(blocks));
     }
-    private IEnumerator Generate(blocks)
+
+    private IEnumerator Generate(List<Block> blocks)
     {
-        foreach(Block B in blocks)
+        foreach (Block B in blocks)
         {
-            //TODO: intantiate the blocks 
-            yield return new WaitForSeconds(time);
+            //TODO: intantiate the blocks
+            yield return new WaitForSeconds(2);
         }
     }
 }
