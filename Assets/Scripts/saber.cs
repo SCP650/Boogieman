5702ef3329f900429fbb3c09965c15e03ae4bff1
Streakman <46490210+Streakman@users.noreply.github.com>
2020-10-20 13:47:37 -0400

    Update saber.cs

--- Assets/Scripts/saber.cs
+++ Assets/Scripts/saber.cs
@@ -16,23 +16,7 @@ public class saber : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       /* RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward,out hit, 1, layer) && hit.transform != null)
-        {
-            rotation = Vector3.Angle(transform.position - previousPos, hit.transform.up);
-            if (hit.transform.gameObject.tag == "beat")
-            {
-                var HisDir = hit.transform.GetComponent<beat>().Dir;
-                Debug.Log("His dir");
-                Debug.Log(HisDir);
-                Debug.Log("My dir");
-                Debug.Log(rotation);
-                if ((HisDir - toleration) <= rotation && rotation <= (HisDir + toleration))//if our hit is at the required angle +- toleration
-                {
-                    Destroy(hit.transform.gameObject);
-                }
-            }
-        }*/
+       //rotation = Vector3.Angle(transform.position - previousPos, other.transform.up);
         previousPos = transform.position;
     }
 
@@ -42,13 +26,13 @@ public class saber : MonoBehaviour
         rotation = Vector3.Angle(transform.position - previousPos, other.transform.up);
         if (other.transform.gameObject.tag == "beat")
         {
-/*            var HisDir = other.transform.GetComponent<beat>().Dir;
-            Debug.Log("His dir");
-            Debug.Log(HisDir);
-            Debug.Log("My dir");
-            Debug.Log(rotation);
-            if ((HisDir - toleration) <= rotation && rotation <= (HisDir + toleration))//if our hit is at the required angle +- toleration
-            {*/
+           //var HisDir = other.transform.GetComponent<beat>().Dir;
+            //Debug.Log("His dir");
+            //Debug.Log(HisDir);
+            //Debug.Log("My dir");
+            print(rotation);
+            //if ((HisDir - toleration) <= rotation && rotation <= (HisDir + toleration))//if our hit is at the required angle +- toleration
+            //{ 
                 Destroy(other.transform.gameObject);
             //}
         }
