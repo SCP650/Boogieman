2f17fe84b644fb056d8b0df559c62f170592757d
Streakman <46490210+Streakman@users.noreply.github.com>
2020-11-09 13:09:20 -0500

    renamed Scenes and Scripts to have an underscore there

---
+++ Assets/_Scripts/SessionHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SessionHandler : MonoBehaviour
+{
+    [SerializeField] List<Session> trueSessions;
+    [SerializeField] List<Session> falseSessions;
+    void Start() 
+    {
+        foreach (Session s in trueSessions) {
+            s.SetToggle(true);
+        }
+
+        foreach (Session s in falseSessions) {
+            s.SetToggle(false);
+        }
+    }
+}
---
+++ Assets/_Scripts/VRResearch/Block.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    public float _time;
+    public int _lineIndex;
+    public int _lineLayer;
+    public int _type;
+    public int _cutDirection;
+    
+}
