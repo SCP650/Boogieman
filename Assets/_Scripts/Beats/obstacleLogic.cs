6c6bcd7e9738c23022ebfe3b1ee86c5f55720981
Streakman <46490210+Streakman@users.noreply.github.com>
2021-02-16 01:55:37 -0500

    backend finished, UI placeholders put in.

---
+++ Assets/_Scripts/Beats/obstacleLogic.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obstacleLogic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Player player = null;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.gameObject.tag == "MainCamera")
+        {
+            Debug.Log("Player's here!");
+            player = other.transform.gameObject.GetComponent<Player>();
+            if (player != null)
+            {
+                player.StartDealingDamage();
+                Messenger.Broadcast("BadHit");
+            }
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.transform.gameObject.tag == "MainCamera")
+        {
+            if (player != null)
+            {
+                Debug.Log("Player's inside t h e  z o n e");
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.gameObject.tag == "MainCamera")
+        {
+            if (player != null)
+            {
+                Debug.Log("Player's gone");
+                player.StopDealingDamage();
+            }
+        }
+    }
+}
--- Assets/_Scripts/PlayerLogic/Player.cs
+++ Assets/_Scripts/PlayerLogic/Player.cs
@@ -8,8 +8,11 @@ public class Player : MonoBehaviour
     private InputManager inputManager;
 
     private Camera m_MainCamera;
-    
-    
+    bool isInObstacle = false;
+    float obstacleDMG = 0.1f;
+    float avoidanceHealth = 100.0f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,12 +24,68 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(avoidanceHealth);
     }
 
-    private void OnCollisionEnter(Collision collision)
+    IEnumerator DamageSelf()
     {
-        Debug.Log("We Hit Something");
+        while (avoidanceHealth > 0)
+        {
+            avoidanceHealth -= obstacleDMG;
+            Debug.Log("hurt player");
+            yield return null;
+        }
+        if (avoidanceHealth <= 1.0f)//is this not needed? 
+        {
+            Messenger.Broadcast("GameOver");
+        }
+        yield return "yoo he dead";
     }
+
+    IEnumerator HealSelf()
+    {
+        while (avoidanceHealth < 100)
+        {
+            if (avoidanceHealth <= 50)
+            {
+                avoidanceHealth = 65;
+            }
+            else
+            {
+                Debug.Log("heal player");
+                avoidanceHealth += obstacleDMG;
+            }
+            yield return null;
+        }
+        yield return "he healed";
+    }
+
+    public void StartDealingDamage()
+    {
+        //Stop a coroutine called HealSelf, if running
+        StopCoroutine(HealSelf());
+        //Start a coroutine called DamageSelf
+        StartCoroutine(DamageSelf());
+
+    }
+
+
+    public void StopDealingDamage()
+    {
+        //Stop a coroutine called DamageSelf, if running
+        StopCoroutine(DamageSelf());
+        //Start a coroutine called HealSelf
+        StartCoroutine(HealSelf());
+    }
+
+    /*    private void OnTriggerEnter(Collider other)
+        {
+            if (other.transform.gameObject.tag == "obstacle")
+            {
+                Debug.Log("We Hit A Wall");
+
+            }
+        }
+    */
 
 }
